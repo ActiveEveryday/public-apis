a5985046447f1a03f50e3a660f552b78095e7ea1
Mohit Paddhariya <mpaddhariya435@rku.ac.in>
2023-08-17 02:43:23 +0530

    It is contains some .env configuratins

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-// use App\Http\Controllers\Apis\V1\UserApiController;
-
-use App\Http\Controllers\Api\v1\UserApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/',[UserApiController::class,'forRecycler']);
+Route::get('/recycler',[UserApiController::class,'forRecycler']);
